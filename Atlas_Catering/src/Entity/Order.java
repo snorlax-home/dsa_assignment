66b35a67fc0d4f5fd2bfc817542c8088560654ab
Amamiya <nengfu14@gmail.com>
2022-09-04 15:50:11 +0800

    add Order entity

---
+++ Atlas_Catering/src/Entity/Order.java
@@ -0,0 +1,11 @@
+package Entity;
+
+public class Order {
+    String orderNo;
+    double orderPrice;
+
+    public Order(String orderNo, double orderPrice){
+        this.orderNo = orderNo;
+        this.orderPrice = orderPrice;
+    }
+}
