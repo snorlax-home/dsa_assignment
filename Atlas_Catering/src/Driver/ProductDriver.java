b8c183e477c34370c109b14590c1e82501bf879a
tjong1 <47622135+tjong1@users.noreply.github.com>
2022-09-11 22:26:58 +0800

    Added functionality to main

--- Atlas_Catering/src/Driver/ProductDriver.java
+++ Atlas_Catering/src/Driver/ProductDriver.java
@@ -7,22 +7,9 @@ import View.ProductView;
 import View.InterfaceUtils;
 
 public class ProductDriver {
-    public static void main(String[] args) {
-        ProductController productController = new ProductController();
+    public static void productMenu(ProductController productController) {
         ProductView productView = new ProductView(productController);
         int choice = 0;
-
-        // Add sample product data
-        productController.addProduct("P001", "Chicken Chop", "Chicken Chop with  fries", 8.00);
-        productController.addProduct("P002", "Nasi Lemak", "Traditional Malay Nasi Lemak", 6.00);
-        productController.addProduct("P003", "Chicken Rice", "Rice with Chicken", 6.00);
-        productController.addProduct("P004", "Nasi Goreng", "Traditional Malay Fried Rice", 6.00);
-        productController.addProduct("P005", "Spaghetti", "Traditional Italian Spaghetti", 8.00);
-        productController.addProduct("P006", "Chinese Fried Rice", "Traditional Chinese Fried Rice", 6.00);
-        productController.addProduct("P007", "Char Kway Teow", "Char Kway Teow with Fried Egg and Prawn", 6.00);
-        productController.addProduct("P008", "Mee Goreng", "Traditional Malay Fried Noodles", 6.00);
-        productController.addProduct("P009", "Wan Tan Mee", "Traditional Chinese Wan Tan Mee", 6.00);
-        productController.addProduct("P010", "Popcorn Chicken Rice", "Rice with Popcorn Chicken and salad", 8.00);
 
         // Display menu
         while (choice != 5) {
@@ -50,7 +37,7 @@ public class ProductDriver {
                     productView.productReport();
                     break;
                 case 5:
-                    System.out.println("Thank you for using Atlas Catering!");
+                    System.out.println("Exiting Product Menu...");
                     break;
             }
         }
--- Atlas_Catering/src/Main.java
+++ Atlas_Catering/src/Main.java
@@ -1,12 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
- *
- * @author TYH
+ * @author Ong Tun Jiun
  */
+
+import Controller.ProductController;
+import Driver.ProductDriver;
+import View.InterfaceUtils;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        ProductController productController = new ProductController();
+
+        productController.addProduct("P001", "Chicken Chop", "Chicken Chop with  fries", 8.00);
+        productController.addProduct("P002", "Nasi Lemak", "Traditional Malay Nasi Lemak", 6.00);
+        productController.addProduct("P003", "Chicken Rice", "Rice with Chicken", 6.00);
+        productController.addProduct("P004", "Nasi Goreng", "Traditional Malay Fried Rice", 6.00);
+        productController.addProduct("P005", "Spaghetti", "Traditional Italian Spaghetti", 8.00);
+        productController.addProduct("P006", "Chinese Fried Rice", "Traditional Chinese Fried Rice", 6.00);
+        productController.addProduct("P007", "Char Kway Teow", "Char Kway Teow with Fried Egg and Prawn", 6.00);
+        productController.addProduct("P008", "Mee Goreng", "Traditional Malay Fried Noodles", 6.00);
+        productController.addProduct("P009", "Wan Tan Mee", "Traditional Chinese Wan Tan Mee", 6.00);
+        productController.addProduct("P010", "Popcorn Chicken Rice", "Rice with Popcorn Chicken and salad", 8.00);
+
+        int choice = 0;
+
+        // Display main menu
+        while (choice != 5) {
+            InterfaceUtils.printHeader();
+            System.out.println("Main Menu");
+            System.out.println("---------");
+            System.out.println("1. Order");
+            System.out.println("2. Products");
+            System.out.println("3. Payment");
+            System.out.println("4. User");
+            System.out.println("5. Exit");
+            System.out.print("Please enter your choice: ");
+            choice = InterfaceUtils.getChoice(5);
+            switch (choice) {
+                case 1:
+                    //OrderDriver.orderMenu();
+                    break;
+                case 2:
+                    ProductDriver.productMenu(productController);
+                    break;
+                case 3:
+                    //PaymentDriver.paymentMenu();
+                    break;
+                case 4:
+                    //UserDriver.userMenu();
+                    break;
+                case 5:
+                    System.out.println("Thank you for using Atlas Catering!");
+                    break;
+            }
+        }
+    }
 }
